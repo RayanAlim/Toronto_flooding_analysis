f777eb68cba8d5e2367be612e3ac9e7f1c28077b
Rayan <rayan.peaceawad@gmail.com>
2024-09-27 17:29:27 +0000

    Update dowload and cleaning scripts

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -14,7 +14,7 @@ library(dplyr)
 library(here)
 
 
-#### Save data ####
+#### Save data - Basement Flooding info ####
 raw_flood_data <- show_package("2241becb-39a5-48fb-9d7a-b65b7a557dce") %>% 
   list_package_resources() %>% 
   filter(row_number() == 1) %>% 
@@ -24,3 +24,21 @@ basement_flooding_by_ward <- raw_flood_data[[1]]
 
 
 write_csv(basement_flooding_by_ward, here("data", "raw_data", "raw_flood_data.csv"))
+
+
+#### Save data- Wards shapefile (Code chunk directly from TO Open Data Site) %>% 
+
+# get package
+package <- show_package("5e7a8234-f805-43ac-820f-03d7c360b588")
+
+# get all resources for this package
+resources <- list_package_resources("5e7a8234-f805-43ac-820f-03d7c360b588")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+wards_shape_file <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+
+write_csv(wards_shape_file, here("data", "raw_data", "wards_shape_file.csv"))
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -4,7 +4,7 @@
 # Date: 24 September 2024
 # Contact: rayan.alim@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Need to download datset by running script `01-donload_data.R`
+# Pre-requisites: Need to download datsets by running script `01-download_data.R`
 # Any other information needed? None
 
 #### Workspace setup ####
@@ -12,30 +12,61 @@ library(tidyverse)
 library(tidyr)
 library(here)
 
-#### Clean data ####
-data <- read_csv(here("data", "raw_data", "raw_flood_data.csv"))
+#### Clean data- Basement flood data ####
+flood_raw_data <-
+  read_csv(here("data", "raw_data", "raw_flood_data.csv"), 
+           col_types = cols(.default = "c"))
 
 # remove unnecessary rows
-data_clean <- data[-1, ]
+flood_data_clean <- flood_raw_data[-1,]
 
 # set correct column names
-colnames(data_clean) <- data[1, ]
+colnames(flood_data_clean) <- flood_raw_data[1,]
 
-data_clean <- slice(data_clean, 1:(n() - 23))
+flood_data_clean <- slice(flood_data_clean, 1:(n() - 23))
 
-# year columns to numeric
-data_clean <- data_clean %>%
-  mutate(across(matches("20[0-9]{2}"), as.numeric))
+names(flood_data_clean)[1] <- "Ward"
 
-# reshaping the data into long format
-data_long <- data_clean %>%
-  pivot_longer(cols = starts_with("20"),
-               names_to = "Year",
-               values_to = "Service_Requests")
+data_long <- flood_data_clean %>%
+  pivot_longer(cols = starts_with("20"), 
+               names_to = "Year", 
+               values_to = "Service_Requests") %>%
+  filter(str_detect(Year, "^\\d+$")) %>%  #filter out the non-numeric year values
+  mutate(Year = as.integer(Year),  
+         Service_Requests = as.integer(Service_Requests), 
+         Ward = as.integer(Ward))  
 
-#  year columns to numeric
-data_long$Year <- as.numeric(data_long$Year)
 
-#### Save data ####
-
+#Save cleaned flood data only: 
 write_csv(data_long, "data/analysis_data/clean_flood_data.csv")
+
+#### Clean data- Wards shapefile ####
+
+# Load the raw data with explicit column types
+shape_raw_data <- read_csv(here("data", "raw_data", "wards_shape_file.csv"), 
+                           col_types = cols(
+                             `_id` = col_double(),
+                             AREA_SHORT_CODE = col_character(),
+                             geometry = col_character()
+                           ))
+
+# Drop unnecessary columns from the shapefile data
+clean_shape_data <- shape_raw_data %>%
+  select(
+    -AREA_ID, -DATE_EFFECTIVE, -DATE_EXPIRY, -AREA_ATTR_ID, -AREA_TYPE_ID, 
+    -PARENT_AREA_ID, -AREA_TYPE, -AREA_CLASS_ID, -AREA_CLASS, -AREA_LONG_CODE, 
+    -AREA_NAME, -FEATURE_CODE, -FEATURE_CODE_DESC, -TRANS_ID_CREATE, 
+    -TRANS_ID_EXPIRE, -OBJECTID
+  )
+
+clean_shape_data <- clean_shape_data %>%
+  mutate(`_id` = as.integer(`_id`))
+
+#Save cleaned shape data only: 
+write_csv(clean_shape_data, "data/analysis_data/clean_shape_data.csv")
+
+merged_data <- clean_shape_data %>%
+  left_join(data_long, by = c("_id" = "Ward"))
+
+#Save cleaned merged data : 
+write_csv(merged_data, file= here("data", "analysis_data", "clean_merged_data.csv"))
